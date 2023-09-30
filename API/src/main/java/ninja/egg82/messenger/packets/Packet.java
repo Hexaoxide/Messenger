569584a9c8c60dcd54ba9a9bf1fcefa25d1cad95
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-09-30 13:08:59 -0700

    remove Serializable from Packet

--- API/src/main/java/ninja/egg82/messenger/packets/Packet.java
+++ API/src/main/java/ninja/egg82/messenger/packets/Packet.java
@@ -3,10 +3,9 @@ package ninja.egg82.messenger.packets;
 import io.netty.buffer.ByteBuf;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.Serializable;
 import java.util.UUID;
 
-public interface Packet extends Serializable {
+public interface Packet {
     void read(@NotNull ByteBuf buffer);
 
     void write(@NotNull ByteBuf buffer);
